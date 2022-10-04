88fb77174bedb8d3586a0085757db893bab266de
Griffin Dooley <dooleygriffin@gmail.com>
2022-10-03 22:52:23 -0400

    playing footsie

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col border-t justify-center items-center py-12">
-      <div>20 West 23rd Street, 3rd Floor New York, NY 10010 (Warrior NYC)</div>
+    <footer className="border-t py-12 w-5/6 m-auto">
+      <div className="flex flex-col justify-center items-center">
+        <div>
+          20 West 23rd Street, 3rd Floor New York, NY 10010 (Warrior NYC)
+        </div>
 
-      <div>Email: info@lanastacyacupuncture.com</div>
+        <div>Email: info@lanastacyacupuncture.com</div>
 
-      <div>Phone: 732-759-4665</div>
+        <div>Phone: 732-759-4665</div>
 
-      <div>IG: @lana.stacy.acupuncture</div>
+        <div>IG: @lana.stacy.acupuncture</div>
+      </div>
     </footer>
   );
 };
