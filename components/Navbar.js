a64d9ae419028f73469a48a15a1a24111a8f9f6c
Griffin Dooley <dooleygriffin@gmail.com>
2022-10-07 15:03:58 -0400

    sticky nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="flex border bg-gray-200 py-6 justify-around">
-      <Link href="/">Home</Link>
+    <nav className="flex border bg-gray-200 py-4 justify-around text-xs sticky top-0">
+      <div className="hover:text-white duration-500">
+        <Link href="/">Home</Link>
+      </div>
       <Link href="/housecalls">House Calls</Link>
       <Link href="/about">About Me</Link>
       <Link href="/services">Services</Link>
--- pages/bookings.js
+++ pages/bookings.js
@@ -1,9 +1,22 @@
 const Bookings = () => {
-    return ( 
-        <div className="h-screen">
-            Book your appointment here
-        </div>
-     );
-}
- 
+  return (
+    <div className="h-screen">
+      Book your appointment here
+      <button
+        className="
+            px-4
+            py-2
+            border-black
+            border-2
+            font-bold
+            hover:bg-black
+            duration-500
+            hover:text-white"
+      >
+        BOOK NOW
+      </button>
+    </div>
+  );
+};
+
 export default Bookings;
